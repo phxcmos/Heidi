753cafbb8ec7d35b97ecaec9c2300e502e92964e
Ken Wang <wirezero@gmail.com>
2018-03-16 06:52:46 -0700

    updates

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {
+    database: 'test',
+    username: 'root',
+    password: 'password',
+    host: 'localhost',
+    port: ''
+};
+
+module.exports = config;
--- controllers/signin.js
+++ controllers/signin.js
@@ -2,10 +2,10 @@ module.exports = {
     'POST /signin': async (ctx, next) => {
         var
             email = ctx.request.body.email || '',
-            password = ctx.resquest.body.password || '';
+            password = ctx.request.body.password || '';
         if (email === 'admin@example.com' && password === '123456') {
             console.log('signin ok!');
-            ctx.render('sigin-ok.html', {
+            ctx.render('sign-ok.html', {
                 title: 'Sign In Ok',
                 name: 'Mr Node'
             });
